d4b5ecdb986c010e66a9e3df03548b134254fe05
Adriano <rodrigues@dee.ufc.br>
2020-11-06 13:41:55 -0300

    Added LTI model

--- model.py
+++ model.py
@@ -2,34 +2,49 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-N = 101
+N = 1001
 
 wo = 0.01
 
-x1 = 0
+x1   = 0
+yLTI1= 0
+u1   = 0
+u2   = 0
 
-x = np.zeros(N)
-y = np.zeros(N)
-t = np.linspace(0, 10, N)
+x     = np.zeros(N)
+yLPV  = np.zeros(N)
+yLTI  = np.zeros(N)
+u     = np.zeros(N)
+theta = np.zeros(N)
+t     = np.linspace(0, 1000, N)
 for k in range(N):
-    theta = 0.5 * np.cos(3*wo*t[k])
-    u     = np.sin(wo*t[k])
-    a0    = -0.78 + 0.44*theta
-    d0    = 0.3 + 0.9*theta
+    theta[k] = 0.5 * np.cos(3*wo*t[k])
+    u[k]     = np.sin(wo*t[k])
+    a0       = -0.78 + 0.44*theta[k]
+    d0       = 0.3 + 0.9*theta[k]
 
     #A = a0 + q
     #B = 1
     #C = q
     #D = d0
 
-    x[k] = 0.78*x1 - 0.44*theta*x1 + u
-    y[k] = 0.3*x1 + 0.9*theta*x1
+    # LPV synthesis
+    x[k]  = (0.78 - 0.44*theta[k])*x1 + u1
+    yLPV[k]  = (0.3 + 0.9*theta[k])*x1
+
+    #LTI synthesis
+    yLTI[k] = (0.78 - 0.44*theta[k])*yLTI1 + (0.3 + 0.9*theta[k])*u2
 
     x1 = x[k]
-print(x)
-print(y)
+    yLTI1 = yLTI[k]
+
+    u2 = u1
+    u1 = u[k]
+    
 fig = plot.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.plot(t,x)
-ax.plot(t,y)
+ax = fig.add_subplot(2, 1, 1)
+ax.plot(t, yLPV)
+ax.plot(t, yLTI)
+ay = fig.add_subplot(2, 1, 2)
+ay.plot(t, theta)
 plot.show()
