caed98289b142092bd25905ceb3535fb868c1943
Adriano <rodrigues@dee.ufc.br>
2020-11-09 11:08:02 -0300

    Added another expression for yLPV as y2LPV

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ N = 1001
 wo = 0.01
 
 x1   = 0
+x2   = 0
 yLTI1= 0
 u1   = 0
 u2   = 0
@@ -14,6 +15,7 @@ u2   = 0
 x     = np.zeros(N)
 yLPV  = np.zeros(N)
 yLTI  = np.zeros(N)
+y2LPV = np.zeros(N)
 u     = np.zeros(N)
 theta = np.zeros(N)
 t     = np.linspace(0, 1000, N)
@@ -31,10 +33,12 @@ for k in range(N):
     # LPV synthesis
     x[k]  = (0.78 - 0.44*theta[k])*x1 + u1
     yLPV[k]  = (0.3 + 0.9*theta[k])*x1
+    y2LPV[k] = (0.3 + 0.9*theta[k])/(0.78 - 0.44*theta[k])*(x[k] - u1)
 
     #LTI synthesis
     yLTI[k] = (0.78 - 0.44*theta[k])*yLTI1 + (0.3 + 0.9*theta[k])*u2
 
+    x2 = x1
     x1 = x[k]
     yLTI1 = yLTI[k]
 
@@ -45,6 +49,7 @@ fig = plot.figure()
 ax = fig.add_subplot(2, 1, 1)
 ax.plot(t, yLPV)
 ax.plot(t, yLTI)
+# ax.plot(t, y2LPV)
 ay = fig.add_subplot(2, 1, 2)
 ay.plot(t, theta)
 plot.show()
